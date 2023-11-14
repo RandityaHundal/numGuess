5dc6bb5d0ced67d030ba9a28bcb034bd7a72b5f1
Randitya Hundal <67023582+RandityaHundal@users.noreply.github.com>
2023-11-13 20:38:44 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿Random numGen = new Random();
+int number = numGen.Next(0,101); 
+
+Console.WriteLine("input a number 1 - 100");
+int guess = Convert.ToInt32(Console.ReadLine());
+
+if (guess == number)
+{
+    
+}
+else
+{
+    while (guess != number)
+    {
+        string status = "tbd";
+        if (guess > number)
+        {
+            status = "Lower, try again";
+        }
+
+        if (guess < number)
+        {
+            status = "Higher, try again";
+        }
+        
+        Console.WriteLine(status);
+        guess = Convert.ToInt32(Console.ReadLine());
+        
+    
+    }
+    
+}
+Console.WriteLine("Congrats you guessed it!");
+
+
+// close by key
+Console.ReadKey();
