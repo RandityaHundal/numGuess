c2f049a04c771d1a5a4527a4a6e4570b2a30a8a9
Randitya Hundal <67023582+RandityaHundal@users.noreply.github.com>
2023-12-29 14:45:53 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,36 +1,51 @@
-﻿Random numGen = new Random();
+Random numGen = new Random();
 int number = numGen.Next(0,101); 
 
-Console.WriteLine("input a number 1 - 100");
-int guess = Convert.ToInt32(Console.ReadLine());
 
-if (guess == number)
+
+Console.WriteLine("Press 1 if you want to play a game.");
+
+int wantPlay = Convert.ToInt32(Console.ReadLine());
+
+if (wantPlay == 1)
 {
+    Console.WriteLine("input a number 1 - 100");
+    int guess = Convert.ToInt32(Console.ReadLine());
     
-}
-else
-{
-    while (guess != number)
+    if (guess == number)
     {
-        string status = "tbd";
-        if (guess > number)
-        {
-            status = "Lower, try again";
-        }
-
-        if (guess < number)
-        {
-            status = "Higher, try again";
-        }
-        
-        Console.WriteLine(status);
-        guess = Convert.ToInt32(Console.ReadLine());
-        
     
     }
+    else
+    {
+        while (guess != number)
+        {
+            string status = "tbd";
+            if (guess > number)
+            {
+                status = "Lower, try again";
+            }
+
+            if (guess < number)
+            {
+                status = "Higher, try again";
+            }
+        
+            Console.WriteLine(status);
+            guess = Convert.ToInt32(Console.ReadLine());
+        
     
+        }
+    
+    }
+    Console.WriteLine("Congrats you guessed it!");
 }
-Console.WriteLine("Congrats you guessed it!");
+
+else
+{
+    Console.WriteLine("Ok, maybe another time.");
+}
+
 
 
 // close by key
